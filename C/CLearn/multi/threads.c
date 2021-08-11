0e5689fd4bcd14d2b9975b092ed71fb079f17839
Diver Dune <sysdeep@yandex.ru>
2021-08-11 09:10:13 +0300

    update c threads example

--- C/CLearn/multi/threads.c
+++ C/CLearn/multi/threads.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h>
 
-
+#define THREADS 10
 
 
 
@@ -11,24 +12,26 @@ void *worker(void *inparams){
     printf("hello [%d] - from thread: %d\n", *params, (int)pthread_self());
     // printf("hello - from thread: %d\n", (int)pthread_self());
 
+	usleep(2 * 1000000);
+	printf("thread: %d - finished\n", *params);
+
 }
+
 
 int main(){
 
-    pthread_t ts[10];
+    pthread_t ts[THREADS];
+	int threads_params[THREADS];			// save params
     
     
 
     for(int i=0; i<10;i++){
-        // pthread_t t;
-        // pthread_create(&t, NULL, worker, NULL);
-        // ts[i] = t;
-        pthread_create(&ts[i], NULL, worker, (void*)&i);
-        // pthread_create(&ts[i], NULL, worker, NULL);
+		threads_params[i] = i;
+        pthread_create(&ts[i], NULL, worker, (void*)&threads_params[i]);
     }
-    
-    for(int i=0; i<10;i++){
 
+
+    for(int i=0; i<10;i++){
         pthread_join(ts[i], NULL);
     }
 
